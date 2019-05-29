c0f67677d2e7ae271c32d6a109b3baa44a944f7f
Raukk <Raukk@users.noreply.github.com>
2019-05-29 10:47:24 -0500

    Experimented with versions of tensorflow in setup and tox

--- setup.py
+++ setup.py
@@ -14,7 +14,25 @@ setup(
     author='Ben Whetton',
     author_email='',
     python_requires='>=3',
-    install_requires=['keras>=2.0.7'],
+    install_requires=['tensorflow>=1.12.0'],
+    extras_require={'examples': ['pandas'], },
+    tests_require=['pytest'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
+
+
+setup(
+    name='tfkerassurgeon-gpu',
+    version="0.2.0",
+    url='https://github.com/Raukk/tf-keras-surgeon',
+    license='MIT',
+    description='A library for performing network surgery on trained tf.Keras '
+                'models. Useful for deep neural network pruning. Requires tensorflow-gpu.',
+    author='Ben Whetton',
+    author_email='',
+    python_requires='>=3',
+    install_requires=['tensorflow-gpu>=1.12.0'],
     extras_require={'examples': ['pandas'], },
     tests_require=['pytest'],
     packages=find_packages('src'),
